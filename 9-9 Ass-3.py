14401d076ec1055d3406e7aa4db759757cadbf59
R. Nithin George Babu <126333960+RNithingeorge@users.noreply.github.com>
2023-10-09 20:54:09 +0530

    Add files via upload

---
+++ 9-9 Ass-3.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+temp = a
+a= b
+b = temp
+print("After swapping:")
+print("First number:", a)
+print("Second number:",b)
