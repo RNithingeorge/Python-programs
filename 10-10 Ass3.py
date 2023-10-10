d9501b473a1ac5dac3de7b2bde255a29aa197694
R. Nithin George Babu <126333960+RNithingeorge@users.noreply.github.com>
2023-10-10 21:45:46 +0530

    Add files via upload

---
+++ 10-10 Ass3.py
@@ -0,0 +1,10 @@
+sno = input("Enter the sno: ")
+sname = input("Enter the sname: ")
+group = input("Enter the sgroup: ")
+s1 = int(input("Enter the marks for subject 1: "))
+s2 = int(input("Enter the marks for subject 2: "))
+s3 = int(input("Enter the marks for subject 3: "))
+total = s1 + s2 + s3
+avg = total / 3
+print("Total marks:", total)
+print("Average marks:", avg)
