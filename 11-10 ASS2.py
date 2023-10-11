3bfd435bb44a48d2effbec5fb5134033ac14967f
R. Nithin George Babu <126333960+RNithingeorge@users.noreply.github.com>
2023-10-11 22:08:12 +0530

    Add files via upload

---
+++ 11-10 ASS2.py
@@ -0,0 +1,5 @@
+i=int(input('enter value:'))
+if(i%2==0):
+    print('i is even')
+else:
+    print('i is odd')
---
+++ 11-10 ASS3.py
@@ -0,0 +1,10 @@
+sno=int(input('enter sno:'))
+sname=input('enter student name:')
+group=input('enter student group:')
+s1=int(input('enter maths marks'))
+s2=int(input('enter phy marks:'))
+s3=int(input('enter cs marks:'))
+if(s1>=40 or s2>=40 or s3>=40):
+    print('Pass')
+else:
+    print('Fail')
