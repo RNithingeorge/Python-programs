01374ac1f049cb6032aacd00d553bf7e12491927
R. Nithin George Babu <126333960+RNithingeorge@users.noreply.github.com>
2023-10-17 23:13:17 +0530

    Add files via upload

---
+++ 17-10 ASS1.py
@@ -0,0 +1,12 @@
+n=int(input())#3
+i=1#4
+c=0
+while i<=n:#4<=3
+    if n%i==0:#3%3
+        c=c+1#2
+    i=i+1
+if c==2:
+    print('prime number')
+else:
+    print('not a prime number')
+        
---
+++ 17-10 ASS2.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+# Initialize an empty string
+result = ""
+
+# Iterate through the range from 1 to n (inclusive)
+for i in range(1, n+1):
+    # Convert the current integer to a string and append it to the result string
+    result += str(i)
+
+print(result)
