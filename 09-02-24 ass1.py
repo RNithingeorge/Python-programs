670ebb1de7d6111b77618d9eb7da6b4a7902932a
R. Nithin George Babu <126333960+RNithingeorge@users.noreply.github.com>
2024-02-09 19:41:11 +0530

    Add files via upload

---
+++ 09-02-24 ass1.py
@@ -0,0 +1,22 @@
+roman_numerals = {"I" : 1,
+                  "V" : 5,
+                  "X" : 10,
+                  "L" : 50,
+                  "C" : 100,
+                  "D" : 500,
+                  "M" : 1000
+                  }
+
+int_value = 0
+
+user_input = input().upper()
+
+for i in range(len(user_input)):
+    if user_input[i] in roman_numerals:
+            if i + 1 < len(user_input) and roman_numerals[user_input[i]] < roman_numerals[user_input[i + 1]]:
+                int_value -= roman_numerals[user_input[i]]
+            else:
+                int_value += roman_numerals[user_input[i]]
+            print( int_value)
+    else:
+        print("Invalid input.")
